345ff98d4bf1bb04f698f2e768517336b5814650
joeylee.lz <joeylee.lz@alibaba-inc.com>
2022-02-14 13:31:55 +0800

    shenandoahgc

--- pipelines/jobs/dragonwell_configurations/jdk11u_pipeline_config.groovy
+++ pipelines/jobs/dragonwell_configurations/jdk11u_pipeline_config.groovy
@@ -36,7 +36,7 @@ class Config11 {
                             "hotspot"   : '--enable-dtrace=auto',
                             "corretto"  : '--enable-dtrace=auto',
                             "SapMachine": '--enable-dtrace=auto',
-                            "dragonwell": '--enable-dtrace=auto --enable-unlimited-crypto --with-jvm-variants=server --with-zlib=system --with-jvm-features=zgc'
+                            "dragonwell": '--enable-dtrace=auto --enable-unlimited-crypto --with-jvm-variants=server --with-zlib=system --with-jvm-features=zgc,shenandoahgc'
                     ]
             ],
 
