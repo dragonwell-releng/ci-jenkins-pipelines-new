17d1e3fea2e5f424e37f187480c2008d4d0a32c0
lvfei.lv <lvfei.lv@alibaba-inc.com>
2022-07-28 20:33:07 +0800

    fix bug when enabletest is false

--- pipelines/build/common/build_base_file.groovy
+++ pipelines/build/common/build_base_file.groovy
@@ -994,7 +994,8 @@ class Builder implements Serializable {
                                                         target: "target/${config.TARGET_OS}/${config.ARCHITECTURE}/${config.VARIANT}/",
                                                         flatten: true
                                                 )
-                                                context.copyArtifacts(
+                                                if (enableTests)
+                                                    context.copyArtifacts(
                                                         projectName: downstreamJobName,
                                                         selector: context.specific("${downstreamJob.getNumber()}"),
                                                         filter: 'workspace/target/AQAvitTaps/*.tap',
