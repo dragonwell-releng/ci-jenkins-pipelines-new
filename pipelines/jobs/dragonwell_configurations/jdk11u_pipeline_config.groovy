ce2e1496c24f9fc16ad695c056778ba6cea8231e
lvfei.lv <lvfei.lv@alibaba-inc.com>
2022-12-13 15:00:39 +0800

    remove unsupported params in riscv build

--- pipelines/jobs/dragonwell_configurations/jdk11u_pipeline_config.groovy
+++ pipelines/jobs/dragonwell_configurations/jdk11u_pipeline_config.groovy
@@ -184,7 +184,7 @@ class Config11 {
                     arch         : 'riscv64',
                     crossCompile : 'x64',
                     buildArgs    : '--cross-compile',
-                    configureArgs: '--disable-ddr --openjdk-target=riscv64-unknown-linux-gnu --with-sysroot=/opt/fedora28_riscv_root'
+                    configureArgs: '--openjdk-target=riscv64-unknown-linux-gnu --with-sysroot=/opt/fedora28_riscv_root'
             ],
             x64AlpineLinux: [
                     os         : 'alpine-linux',
